50cb1723b46bd05d77e4a4ff227234e4e743c8ee
JigmePS <jigmesherpa015@gmail.com>
2024-05-25 21:57:40 +0545

    Login css update

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import ViPasswordInput from "../../components/ViPasswordInput";
 import ViTextInput from "../../components/ViTextInput";
+import "./Login.css";
 
 const Login = () => {
 
