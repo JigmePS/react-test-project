0db3032786253b44b86a503cbc992627ef9788f9
JigmePS <jigmesherpa015@gmail.com>
2024-05-29 10:11:58 +0545

    update

--- src/pages/theme/Layout.js
+++ src/pages/theme/Layout.js
@@ -28,65 +28,65 @@ function Layout() {
     },
     {
       id: 3,
-      firstName: 'Rolf',
-      lastName: 'Hegdal',
-      email: 'rolf@example.com',
+      firstName: 'Isaac',
+      lastName: 'Limbu',
+      email: 'limbu@example.com',
       salary: '79000',
       date: '2019-05-01'
     },
     {
       id: 4,
-      firstName: 'Kent',
-      lastName: 'Rosner',
-      email: 'kent@example.com',
+      firstName: 'Jack',
+      lastName: 'Ma',
+      email: 'jma@example.com',
       salary: '56000',
       date: '2019-05-03'
     },
     {
       id: 5,
-      firstName: 'Arsenio',
-      lastName: 'Grant',
-      email: 'arsenio@example.com',
+      firstName: 'Henry',
+      lastName: 'Cavil',
+      email: 'warhammer@example.com',
       salary: '65000',
       date: '2019-06-13'
     },
     {
       id: 6,
-      firstName: 'Laurena',
-      lastName: 'Lurie',
-      email: 'laurena@example.com',
+      firstName: 'Steve',
+      lastName: 'Jobs',
+      email: 'sjobs@example.com',
       salary: '120000',
       date: '2019-07-30'
     },
     {
       id: 7,
       firstName: 'George',
-      lastName: 'Tallman',
+      lastName: 'Washington',
       email: 'george@example.com',
       salary: '90000',
       date: '2019-08-15'
     },
     {
       id: 8,
-      firstName: 'Jesica',
-      lastName: 'Watlington',
-      email: 'jesica@example.com',
+      firstName: 'Ash',
+      lastName: 'Ketchup',
+      email: 'pallet@example.com',
       salary: '60000',
       date: '2019-10-10'
     },
     {
       id: 9,
       firstName: 'Matthew',
-      lastName: 'Warren',
-      email: 'matthew@example.com',
+      lastName: 'Murdock',
+      email: 'dared@example.com',
       salary: '71000',
       date: '2019-10-15'
     },
     {
       id: 10,
-      firstName: 'Lyndsey',
-      lastName: 'Follette',
-      email: 'lyndsey@example.com',
+      firstName: 'Royal',
+      lastName: 'Ranger',
+      email: 'ranger@example.com',
       salary: '110000',
       date: '2020-01-15'
     }
